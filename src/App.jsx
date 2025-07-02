6b112cafc04b4887637b789f9ff99a0b4a8fd693
Nafim Ahmed <recentnafimahmed@gmail.com>
2025-07-02 14:19:47 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Button, Flex } from 'antd';
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-amber-300">Hello</h1>
+        <Button type="primary">Primary Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
