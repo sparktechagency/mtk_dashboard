90703773dfd9a91afb3e8da7768f7e86f95b12c0
goni715 <halesgoni715149123@gmail.com>
2025-10-20 10:44:45 +0600

    update submit button

--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -5,14 +5,16 @@ type TProps = {
     isLoading: boolean;
     loadingTitle?: string;
     children: React.ReactNode;
+    disabled?: boolean
 }
 
-const SubmitButton = ({ isLoading, loadingTitle="Processing...", children}: TProps) => {
+const SubmitButton = ({ isLoading, loadingTitle="Processing...", children, disabled=false}: TProps) => {
   return (
     <>
           <button
               type="submit"
-              className="w-full flex justify-center items-center gap-x-2 bg-primary hover:bg-primary/80 cursor-pointer text-white py-2 rounded-md font-semibold transition-colors duration-100"
+              disabled={isLoading || disabled}
+              className="w-full flex justify-center items-center gap-x-2 bg-primary hover:bg-primary/80 cursor-pointer text-white py-2 rounded-md font-semibold transition-colors duration-100 disabled:cursor-not-allowed"
           >
               {isLoading ? (
                   <>
